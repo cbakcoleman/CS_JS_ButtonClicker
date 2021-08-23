af85fc7225a1b137acf6511cfa826842b92d6d6f
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-08-23 14:19:39 -0700

    add js for button interaction

---
+++ script.js
@@ -0,0 +1,15 @@
+function logout(e) {
+    e.innerText = "Logout"
+}
+
+function remove(e) {
+    e.remove();
+}
+
+function liked(e) {
+    window.alert("Ninja was liked")
+}
+
+function chartreuse(e) {
+    window.alert("Chartreuse is an eco-friendly color because you always 're-use' 'Chart!'")
+}
